fbf3322635df062f4075107633758e7e0bb6d70c
dunghenry <trandungksnb6930082001>
2022-10-08 17:37:20 +0700

    add env and build docker

--- main.go
+++ main.go
@@ -5,34 +5,46 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
 type Todo struct {
-	Id        primitive.ObjectID  `json:"id" bson:"_id"`
-    Title     string  	          `json:"title"`
-    Des       string 		      `json:"des"`
-    Completed bool 	              `json:"completed"`
+	Id        primitive.ObjectID `json:"id" bson:"_id"`
+	Title     string             `json:"title"`
+	Des       string             `json:"des"`
+	Completed bool               `json:"completed"`
 }
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	router := gin.Default()
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel();
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil { 
+	defer cancel()
+	// production
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
 		log.Fatal(err)
 	}
 	collection := client.Database("golang").Collection("todos")
-
+	fmt.Println("Connect MongoDB")
 	//Get all todos
-	router.GET("/todos", func(c *gin.Context){
+	router.GET("/todos", func(c *gin.Context) {
 		cur, err := collection.Find(context.Background(), bson.D{})
-		if err != nil { log.Fatal(err) }
+		if err != nil {
+			log.Fatal(err)
+		}
 		defer cur.Close(context.Background())
 
 		var results []Todo
@@ -41,36 +53,36 @@ func main() {
 		}
 		c.JSON(http.StatusOK, gin.H{
 			"status": "success",
-			"todos": results,
+			"todos":  results,
 		})
 	})
 
 	//Create new todo
-	router.POST("/todos", func(c *gin.Context){
+	router.POST("/todos", func(c *gin.Context) {
 		var newTodo Todo
 		newTodo.Id = primitive.NewObjectID()
 		if err := c.BindJSON(&newTodo); err != nil {
 			return
 		}
 		res, err := collection.InsertOne(context.Background(), newTodo)
-		if err != nil { 
+		if err != nil {
 			log.Fatal(err)
-		 }
+		}
 		id := res.InsertedID
 		fmt.Println(res)
 		c.JSON(http.StatusCreated, gin.H{
 			"status": "success",
-			"_id": id,
+			"_id":    id,
 		})
 	})
 
 	//Get todo by id
-	router.GET("/todos/:id", func(c *gin.Context){
+	router.GET("/todos/:id", func(c *gin.Context) {
 		var id = c.Param("id")
 		todoIdObject, err := primitive.ObjectIDFromHex(id)
 		if err != nil {
 			c.JSON(http.StatusOK, gin.H{
-				"status": "failure",
+				"status":  "failure",
 				"message": "Id invalid",
 			})
 			return
@@ -78,25 +90,25 @@ func main() {
 		var result Todo
 		err = collection.FindOne(context.TODO(), bson.D{{"_id", todoIdObject}}).Decode(&result)
 		if result.Title == "" && result.Des == "" {
-				c.JSON(http.StatusNotFound, gin.H{
-				"status": "failure",
+			c.JSON(http.StatusNotFound, gin.H{
+				"status":  "failure",
 				"message": "Todo not found!",
 			})
 			return
-		}else{
-				c.JSON(http.StatusOK, gin.H{
+		} else {
+			c.JSON(http.StatusOK, gin.H{
 				"status": "success",
-				"item": result,
+				"item":   result,
 			})
 			return
 		}
 	})
-	router.DELETE("/todos/:id", func(c *gin.Context){
+	router.DELETE("/todos/:id", func(c *gin.Context) {
 		var id = c.Param("id")
 		todoIdObject, err := primitive.ObjectIDFromHex(id)
 		if err != nil {
 			c.JSON(http.StatusOK, gin.H{
-				"status": "failure",
+				"status":  "failure",
 				"message": "Id invalid",
 			})
 			return
@@ -104,14 +116,14 @@ func main() {
 		var result Todo
 		err = collection.FindOneAndDelete(context.TODO(), bson.D{{"_id", todoIdObject}}).Decode(&result)
 		if result.Title == "" && result.Des == "" {
-				c.JSON(http.StatusNotFound, gin.H{
-				"status": "failure",
+			c.JSON(http.StatusNotFound, gin.H{
+				"status":  "failure",
 				"message": "Todo not found!",
 			})
 			return
-		}else{
-				c.JSON(http.StatusOK, gin.H{
-				"status": "success",
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"status":  "success",
 				"message": "Deleted todo successfully!",
 			})
 			return
@@ -119,7 +131,7 @@ func main() {
 	})
 
 	// Update todo by id
-	router.PUT("todos/:id", func(c *gin.Context){
+	router.PUT("todos/:id", func(c *gin.Context) {
 		var id = c.Param("id")
 		todoIdObject, err := primitive.ObjectIDFromHex(id)
 		var updateTodo Todo
@@ -129,7 +141,7 @@ func main() {
 		fmt.Println(updateTodo.Title)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
-				"status": "failure",
+				"status":  "failure",
 				"message": "Id invalid",
 			})
 			return
@@ -137,31 +149,31 @@ func main() {
 		var result Todo
 		err = collection.FindOne(context.TODO(), bson.D{{"_id", todoIdObject}}).Decode(&result)
 		if result.Title == "" && result.Des == "" {
-				c.JSON(http.StatusNotFound, gin.H{
-				"status": "failure",
+			c.JSON(http.StatusNotFound, gin.H{
+				"status":  "failure",
 				"message": "Todo not found!",
 			})
 			return
-		}else{
-				opts := options.Update().SetUpsert(true)
-				result, err := collection.UpdateOne(context.TODO(), bson.D{{"_id", todoIdObject}}, bson.D{{"$set", bson.D{{"title", updateTodo.Title}, {"des", updateTodo.Des},{"completed", updateTodo.Completed}}}},opts)
-				if err != nil {
-					log.Fatal(err)
-				}
-				if result.MatchedCount != 0 {
-					c.JSON(http.StatusOK, gin.H{
-						"status": "success",
-						"item": "Updated todo successfully!",
-					})
-					return
-				}else{
-					c.JSON(http.StatusBadRequest, gin.H{
-						"status": "failure",
-						"message": "Todo update failure!",
-					})
-					return
-				}
+		} else {
+			opts := options.Update().SetUpsert(true)
+			result, err := collection.UpdateOne(context.TODO(), bson.D{{"_id", todoIdObject}}, bson.D{{"$set", bson.D{{"title", updateTodo.Title}, {"des", updateTodo.Des}, {"completed", updateTodo.Completed}}}}, opts)
+			if err != nil {
+				log.Fatal(err)
 			}
-		})
-	router.Run("localhost:3000")
+			if result.MatchedCount != 0 {
+				c.JSON(http.StatusOK, gin.H{
+					"status": "success",
+					"item":   "Updated todo successfully!",
+				})
+				return
+			} else {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"status":  "failure",
+					"message": "Todo update failure!",
+				})
+				return
+			}
+		}
+	})
+	router.Run(":3000")
 }
